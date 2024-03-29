92cf99f12764374f8eb988380dd840192f6e5df4
akshay343 <127043464+Akshay-343@users.noreply.github.com>
2024-03-29 21:37:39 +0530

    Update settings.py

--- OnlineShopping/settings.py
+++ OnlineShopping/settings.py
@@ -109,10 +109,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-
 MEDIA_ROOT = os.path.join(BASE_DIR, 'shop/media')
 MEDIA_URL = '/media/'
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static")
 ]
+
+# New setting for production use
+STATIC_ROOT = '/static/'
